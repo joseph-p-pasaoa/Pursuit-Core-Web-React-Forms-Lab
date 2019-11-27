70ee2a21764b682a9eb9bf7e943387a536e5cba7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-11-27 15:49:58 -0500

    0.1, packed README, init react architecture

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/*
+Joseph P. Pasaoa
+React with Forms Lab
+*/
+
+
+/* IMPORTS */
+import React from 'react';
+import './App.css';
+
+
+/* APP */
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+
+/* EXPORT */
+export default App;
