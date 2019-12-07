47ee0979b24666d75ecbb075101d6e2befb1485b
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-12-07 15:10:05 -0500

    0.3, bulk of app.js jsx coded

--- src/App.js
+++ src/App.js
@@ -11,11 +11,51 @@ import './App.css';
 
 /* APP */
 class App extends React.Component {
+  constructor() {
+    super();
+    this.initialState = {
+      txtValue: "",
+      selectValue: ""
+    }
+    this.state = this.initialState;
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+  }
 
   render() {
+    const { txtValue, selectValue } = this.state;
+
     return (
       <div className="App">
-
+        <h1>Number Array Manipulator</h1>
+        <p>Enter numbers below, separated by commas. Spaces not required.</p>
+        <form id="formNumArr" onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            id="txtValue" 
+            name="txtValue" 
+            value={txtValue} 
+            onChange={this.handleChange} 
+            placeholder="Enter numbers here" 
+            autoFocus
+          />
+          <select id="selectValue" name="selectValue" value={selectValue} onChange={this.handleChange}>
+            <option value="" disabled>-- Choose an operation</option>
+            <option value="sum">sum</option>
+            <option value="average">average</option>
+            <option value="mode">mode</option>
+          </select>
+          <p id="msg">...</p>
+          <button id="btnNumArr">Calculate</button>
+        </form>
       </div>
     );
   }
